a7338d2a60dbbb9f050108f15b8aa7c4b5ec78f5
Felipe <felipedantas184@gmail.com>
2021-07-29 15:36:27 -0300

    Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,12 @@ class MyDocument extends Document {
     return (
       <Html lang="pt-br">
         <Head>
+            <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-C2SBEMPMN3');`}}></script>
+
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
@@ -15,6 +21,8 @@ class MyDocument extends Document {
             <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         </Head>
         <body>
+          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-C2SBEMPMN3"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
           <Main />
           <NextScript />
         </body>
